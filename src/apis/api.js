83eaac281c7c2377faef38be05efc8ee1c8c6d09
combikms <combikms@naver.com>
2024-07-26 20:55:21 +0900

    :sparkles: 게시글 작성 (POST)

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // 백앤드 배포되면 URL 수정하기
 
+// SELECT * from post
 const getPosts = async () => {
     try {
         const result = await axios.get('http://localhost:8080/');
@@ -12,6 +13,7 @@ const getPosts = async () => {
     }
 }
 
+// INSERT (title, content) into post
 const addPost = async (post) => {
     try {
         const result = await axios.post('http://localhost:8080/', post);
--- src/pages/PostWrite.js
+++ src/pages/PostWrite.js
@@ -1,17 +1,46 @@
+import React, { useState } from 'react';
+import { addPost } from '../apis/api';
+
 function PostWrite() {
+
+    // 잘 되긴 하는데 방법이 좀 이상하다.. 최적화가 필요할 듯
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await addPost({ title, content });
+            setTitle('');
+            setContent('');            
+        } catch (error) {
+            console.error('Error adding post:', error);
+        }
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>제목</label>
-                <input type="text" />
+                <input
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                />
             </div>
             <div>
                 <label>내용</label>
-                <textarea></textarea>
+                <textarea
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    required
+                ></textarea>
             </div>
             <button type="submit">작성</button>
         </form>
-    )
+    );
 }
 
 export default PostWrite;
