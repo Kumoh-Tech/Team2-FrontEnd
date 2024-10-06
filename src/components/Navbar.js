7265386c009f374570910f0fbe4ec3ae055258d8
Inseok Kang <combikms@naver.com>
2024-10-06 17:41:01 +0900

    :fire: Delete: 수강신청, 시간표, 강의실 버튼

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import useStore from '../store/store.js';
-import '../styles/Navbar.css'; 
+import '../styles/Navbar.css';
 
 function Navbar() {
     const { user } = useStore();
     return (
         <div className="black-nav">
-            <Link to="/" className='nav-btn'>게시판</Link>
-            <Link to="/timetable" className='nav-btn'>시간표</Link>
-            <Link to="/lecture" className='nav-btn'>강의실</Link>
-            <Link to="/sugang" className='nav-btn'>수강신청</Link>
+            <Link to="/" className='nav-btn'>Kumoh Coders</Link>
             <div className="ml-auto">
-                <span>{user.isLoggedIn ? user.userInfo : "로그인"} </span>
+                <span> {
+                    user.isLoggedIn ?
+                        <Link to="#" className='nav-btn'> 로그아웃 </Link> :
+                        <Link to="/login" className='nav-btn'> 로그인 </Link>
+                }
+                </span>
             </div>
         </div>
     );
